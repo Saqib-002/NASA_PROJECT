94fd489cb964f0b1dc3dcffeb9baefc31590aef7
Saqib <saqibshah2882@gmail.com>
2022-09-03 01:06:56 +0500

    validated requested data

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -9,7 +9,18 @@ function httpGetAllLaunches(req,res){
 
 function httpAddNewLaunch(req,res){
     const launch=req.body;
+    if(!launch.mission || !launch.launchDate || !launch.rocket || !launch.destination){
+        return res.status(400).json({
+            error: 'No launch date provided',
+        })
+    }
     launch.launchDate=new Date( launch.launchDate)
+    // if(isNaN(launch.launchDate)){ same as below
+    if(launch.launchDate.toString()==="Invalid Date"){
+        return res.status(400).json({
+            error: 'Invalid launch date'
+        })
+    }
     addNewLaunch(launch)
     return res.status(201).json(launch)
 }
