deec89de1f5576529d4fc72c7929697277ea1559
Saqib <saqibshah2882@gmail.com>
2022-09-02 10:05:20 +0500

    serve react application through server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,19 @@
 const express=require("express");
 const cors=require("cors");
+const path=require("path");
 const planetsRouter = require("./routes/planets/planets.router")
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"..","public ")))
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join(__dirname,"..","public ","index.html"))   
+})
+console.log(path.join(__dirname,"..","public","index.html"))
+
 app.use(planetsRouter)
 
 module.exports=app;
