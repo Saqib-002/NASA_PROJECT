b3e960450973873f7c2cc287f023442592456da8
Saqib <saqibshah2882@gmail.com>
2022-09-02 14:16:04 +0500

    Added: get launches

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const cors=require("cors");
 const path=require("path");
 const morgan=require("morgan");
 const planetsRouter = require("./routes/planets/planets.router")
+const launchesRouter = require("./routes/launches/launches.router")
 
 const app = express();
 
@@ -17,8 +18,8 @@ app.use(express.static(path.join(__dirname,"..","public ")))
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname,"..","public ","index.html"))   
 })
-console.log(path.join(__dirname,"..","public","index.html"))
 
-app.use(planetsRouter)
+app.use(planetsRouter);
+app.use(launchesRouter);
 
 module.exports=app;
