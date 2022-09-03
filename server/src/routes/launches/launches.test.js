266f3f7204b5101444abc144a606a39c24987b5d
Saqib <saqibshah2882@gmail.com>
2022-09-03 14:34:30 +0500

    Aded: test for GET /launches

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,13 @@
+const request=require("supertest")
+const app=require("../../app")
+
 describe("Test GET /launches",()=>{
-    test("it should respond with 200 success",()=>{
-        const response=200;
-        expect(response).toBe(200)
+    test("it should respond with 200 success",async ()=>{
+        const response=await request(app)
+        .get('/launches')
+        // .expect(200);
+        .expect("Content-Type", /json/);
+        // expect(response.statusCode).toBe(200)
     })
 });
 
