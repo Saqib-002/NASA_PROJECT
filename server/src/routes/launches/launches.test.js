aac3702e1ca6c69b93df18ba395376b111e29c53
Saqib <saqibshah2882@gmail.com>
2022-09-03 16:33:41 +0500

    Added POST /launch test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,7 +5,7 @@ describe("Test GET /launches",()=>{
     test("it should respond with 200 success",async ()=>{
         const response=await request(app)
         .get('/launches')
-        // .expect(200);
+        .expect(200)
         .expect("Content-Type", /json/);
         // expect(response.statusCode).toBe(200)
     })
@@ -13,7 +13,28 @@ describe("Test GET /launches",()=>{
 
 
 describe("Test POST /launches",()=>{
-    test("it should respond with 200 success",()=>{
+    const completeLaunchData={
+        mission:"USS Enterprise",
+        rocket:"NCC 1701-D",
+        target:"Kepler-186 f",
+        launchDate:"January 4, 2028"
+    }
+    const launchDataWithoutDate={
+        mission:"USS Enterprise",
+        rocket:"NCC 1701-D",
+        target:"Kepler-186 f"
+    }
+    test("it should respond with 201 created",async ()=>{
+        const response=await request(app)
+        .post('/launches')
+        .send(completeLaunchData)
+        .expect(201)
+        .expect("Content-Type", /json/)
+
+        const requestDate=new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate=new Date(response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate)
+        expect(response.body).toMatchObject(launchDataWithoutDate);
     })
     test("it should catch missing rerquired properties",()=>{
     })
